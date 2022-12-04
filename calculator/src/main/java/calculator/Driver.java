5dd027c29ca08a80f812b364bc7afcb3e43f8d44
Gondal Mahid (2020) ZJAC351 <mahid.gondal.2020@live.rhul.ac.uk>
2022-12-04 19:41:27 +0000

    Revert "Merge branch 'main' into Feature_GUI"
    This reverts commit 6b69ac3961ae90b4d35ce2045ee178af45435bec

--- calculator/src/main/java/calculator/Driver.java
+++ calculator/src/main/java/calculator/Driver.java
@@ -1,38 +1,21 @@
 package calculator;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+public class Driver extends Application {
 
-/**
- * @author Mahid
- * 
- * This program reads a list of animals from a resource called animals.txt and prints it to the terminal.
- *
- */
-public class Driver {
-
-  /**
-   * The entry point for my animal lister program.
-   * @param args the arguments enetred on the command line for this program (ignored).
-   * @throws IOException thrown when we fail to read animals from the open animals.txt file.
-   */
-  public static void main(String[] args) throws IOException {
-    InputStream is = Driver.class.getResourceAsStream("/animals.txt");
-    Animals animals = new Animals();
-    if (is != null) {
-      BufferedReader br = new BufferedReader(new InputStreamReader(is));
-      String line = br.readLine();
-      while (line != null) {
-        animals.add(line);
-        line = br.readLine();
-      }
-    } else {
-      animals.add("Elephant");
-    }
-    System.out.println(animals);
+  public static void main(String[] args) {
+    launch(args);
   }
 
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("myView.fxml"));
+    Scene scene = new Scene(root, 800, 500);
+    primaryStage.setScene(scene);
+    primaryStage.show();    
+  }
 }
-
---
+++ calculator/src/main/java/calculator/MyView.java
@@ -0,0 +1,131 @@
+package calculator;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class MyView {
+  
+
+    @FXML
+    private Parent root2;
+    
+    @FXML
+    private Button calculateButton;
+
+    @FXML
+    private TextField inputBox;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    private Label message;
+
+    @FXML
+    private Button modeGUI;
+
+    @FXML
+    private Button modeText;
+
+    @FXML
+    private Button reverseButton;
+
+    @FXML
+    private MenuItem viewAll;
+
+    @FXML
+    private MenuItem viewPrevious;
+    
+    @FXML
+    private RadioButton r1;
+
+    @FXML
+    private RadioButton r2;
+    
+    @FXML
+    public void setModeText(ActionEvent event) throws Exception {
+      Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("textView.fxml"));
+      Stage window = (Stage) ((Node) modeText).getScene().getWindow();
+      window.setScene(new Scene(root, 800, 500));
+      window.show();
+    }
+    
+    @FXML
+    public void setModeGUI(ActionEvent event) throws Exception {
+      Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("myView.fxml"));
+      Stage window = (Stage) ((Node) modeGUI).getScene().getWindow();
+      window.setScene(new Scene(root, 800, 500));
+      window.show();
+    }
+
+    @FXML
+    void calcPressed(ActionEvent event) {
+      double r = label.getRotate();
+      label.setRotate(r + 90);
+      String msg = inputBox.getText();
+      message.setText(msg);
+      
+
+    }
+
+    @FXML
+    void historyView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openGUI(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openText(ActionEvent event) {
+
+    }
+
+    @FXML
+    void previousView(ActionEvent event) {
+
+    }
+//
+//    @FXML
+//    void revsersePress(ActionEvent event) {
+//      double r = label.getRotate();
+//      label.setRotate(r - 90);
+//      String msg = inputBox.getText();
+//      message.setText(new StringBuilder(msg).reverse().toString());
+//    }
+//
+
+
+    @FXML
+    void viewHistory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewLast(ActionEvent event) {
+
+    }
+    
+
+    @FXML
+    void isPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void r2IsPressed(ActionEvent event) {
+
+    }
+
+}
