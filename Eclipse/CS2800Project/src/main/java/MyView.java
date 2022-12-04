a37e660b6072bd63442c2201c640c65585062e8a
Mahid Gondal <ZJAC351@live.rhul.ac.uk>
2022-12-04 18:54:54 +0000

    Revert "refactor[project files]: moved all files to calcualtor project"
    This reverts commit 9539645f0f0412159870291d448901340b4480be.

--- Eclipse/CS2800Project/src/main/java/MyView.java
+++ Eclipse/CS2800Project/src/main/java/MyView.java
@@ -1,4 +1,3 @@
-package calculator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
