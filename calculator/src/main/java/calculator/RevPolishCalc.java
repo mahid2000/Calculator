afe292a7fb15e2099fd239176dcf79976a52ba8f
Mahid Gondal <ZJAC351@live.rhul.ac.uk>
2022-12-07 18:02:28 +0000

    feat[RevPolClac]: create class RevPolishCalc implementing Calculator

---
+++ calculator/src/main/java/calculator/RevPolishCalc.java
@@ -0,0 +1,19 @@
+package calculator;
+
+/**
+ * Reverse Polish Notation implementation of Calculator.
+ *
+ * @author Mahid
+ *
+ */
+public class RevPolishCalc implements Calculator {
+
+  @Override
+  public float evaluate(String calculate) throws InvalidExpressionException {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+
+
+}
