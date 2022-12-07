b7b666f233cf3cf8bbc35b1d40b7bb659a72240d
Mahid Gondal <ZJAC351@live.rhul.ac.uk>
2022-12-07 18:06:58 +0000

    feat[RevPolCalc]: implement constructor to create a NumStack
    This constructor creates a new NumStack which will act as the staging
    ground for the expression evaluation in reverse polish notation.

--- calculator/src/main/java/calculator/RevPolishCalc.java
+++ calculator/src/main/java/calculator/RevPolishCalc.java
@@ -8,12 +8,23 @@ package calculator;
  */
 public class RevPolishCalc implements Calculator {
 
+  @SuppressWarnings("unused")
+  private NumStack ram;
+  
+  /**
+   * Constructor for Calculator's memory Stack.
+   *
+   */
+  public RevPolishCalc() {
+    this.ram = null;
+  }
+
   @Override
   public float evaluate(String calculate) throws InvalidExpressionException {
     // TODO Auto-generated method stub
     return 0;
   }
-
+  
 
 
 }
