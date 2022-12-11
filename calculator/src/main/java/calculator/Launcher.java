3d6674253d6df2358516494b5d3a42d2afa5a8ba
Mahid Gondal <ZJAC351@live.rhul.ac.uk>
2022-12-11 04:26:36 +0000

    feat[Launcher]: create launcher class to allow for jar file compilation

---
+++ calculator/src/main/java/calculator/Launcher.java
@@ -0,0 +1,23 @@
+package calculator;
+
+/**
+ * Launcher class used to launch the application (this is needed to compile the project as a
+ * runnable jar file.
+ *
+ * @author Marco Cheung
+ *
+ */
+public class Launcher {
+
+  /**
+   * Main method which calls the main method in Driver and passes it the application class to be
+   * run.
+   *
+   * @param args Application class to be launched
+   */
+  public static void main(String[] args) {
+    Driver.main(args);
+
+  }
+
+}
