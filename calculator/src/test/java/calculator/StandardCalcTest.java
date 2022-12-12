f2235740d9e4e1fb94df0a2796afb671ba58336e
Mahid Gondal <ZJAC351@live.rhul.ac.uk>
2022-12-12 07:25:22 +0000

    feat[StandardCalc]: TDD test to check is InvalidException thrown at null
    evaluate() should throw an InvalidExpressionException if String
    expression is null.

---
+++ calculator/src/test/java/calculator/StandardCalcTest.java
@@ -0,0 +1,34 @@
+package calculator;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Random;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StandardCalcTest {
+
+  private Calculator tester;
+  private Random random = new Random();
+
+  @BeforeEach
+  void setUp() {
+    tester = new StandardCalc();
+  }
+
+  // Test 1: Test to see if evaluate() throws InvalidExpressionException when input is null
+  @Test
+  void testNull() {
+    assertThrows(InvalidExpressionException.class, () -> tester.evaluate(""),
+        "the calculator should throw a InvalidExpressionException if the expression is empty");
+    assertThrows(InvalidExpressionException.class, () -> tester.evaluate(null),
+        "the calculator should throw a InvalidExpressionException if the expression is null");
+  }
+
+
+
+
+
+}
